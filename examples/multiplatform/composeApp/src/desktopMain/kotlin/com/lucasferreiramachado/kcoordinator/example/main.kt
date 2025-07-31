1f65635b178fb9cde714d2a82034d2ce39724d46
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2025-07-31 13:50:22 -0300

    example(multiplatform): Add hot-reload enhancements. Define custom screen size to desktop app. Update dependencies.

--- examples/multiplatform/composeApp/src/desktopMain/kotlin/com/lucasferreiramachado/kcoordinator/example/main.kt
+++ examples/multiplatform/composeApp/src/desktopMain/kotlin/com/lucasferreiramachado/kcoordinator/example/main.kt
@@ -1,13 +1,19 @@
 package com.lucasferreiramachado.kcoordinator.example
+import androidx.compose.ui.unit.DpSize
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.lucasferreiramachado.kcoordinator.example.multiplatform.app.App
 
 fun main() = application {
     Window(
+        state = WindowState(
+            size = DpSize(width = 400.dp, height = 800.dp),
+        ),
         onCloseRequest = ::exitApplication,
-        title = "KCoordinator Example",
+        title = "KCoordinator Example"
     ) {
         App()
     }
